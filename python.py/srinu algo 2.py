7a12065402a728646d9367da812dbe466f7b20a9
SRINU <122525765+srinu0625@users.noreply.github.com>
2024-07-03 19:58:29 +0530

    Update srinu algo 2.py
    change

--- python.py/srinu algo 2.py
+++ python.py/srinu algo 2.py
@@ -21,18 +21,29 @@ low_column_name  = 'Low'
 time_column_name = 'Date (GMT)'
 
 # temp column names
-temp_high = 0
-temp_low  = 0
+temp_high1 = 0
+temp_low1 = 0
+temp_high2 = 0
+temp_low2 = 0
+temp_high3 = 0
+temp_low3 = 0
+prev_local_high1 = 0
+prev_local_low1 = 0
+prev_local_high2 = 0
+prev_local_low2 = 0
+prev_local_high3 = 0
+prev_local_low3 = 0
+
 
 # local high and local low 
 current_high1 = 0
 current_low1  = 0
-local_high1 = temp_high
-local_low1  = temp_low
-local_high2 = temp_high
-local_low2  = temp_low
-local_high3 = temp_high
-local_low3  = temp_low
+local_high1 = temp_high1
+local_low1  = temp_low1
+local_high2 = temp_high2
+local_low2  = temp_low2
+local_high3 = temp_high3
+local_low3  = temp_low3
 
 # flag 
 bull = False
@@ -134,8 +145,8 @@ for index1, row1 in data1.iterrows():
                                 # Printing data for data1
 
                                 print("---60 MIN---:", current_time1)
-                                print("Current High1 :", current_high1, "Previous High1 :", previous_high1, "local_high1 :", local_high1, " prev_local_high1 :", prev_local_high1)
-                                print("Current Low1 :", current_low1, "Previous Low1 :", previous_low1, "local_low1 :", local_low1, " prev_local_low1 :", prev_local_low1)
+                                print("Current High1 :", current_high1, "Previous High1 :", previous_high1, "local_high1 :", local_high1, " prev_local_high1 :", prev_local_high1,"temp_high1 :",temp_high1)
+                                print("Current Low1 :", current_low1, "Previous Low1 :", previous_low1, "local_low1 :", local_low1, " prev_local_low1 :", prev_local_low1,"temp_low1 :",temp_low1)
                                 print("   ")
                                 time.sleep(0.5)
 
@@ -157,8 +168,8 @@ for index1, row1 in data1.iterrows():
                                 # Printing data for data2
 
                                 print("---240 MIN---:", current_time2)
-                                print("Current High2 :", current_high2, "Previous High2 :", previous_high2, "local_high2 :", local_high2, " prev_local_high2:", prev_local_high2)
-                                print("Current Low2 :", current_low2, "Previous Low2 :", previous_low2, "local_low2 :", local_low2, " prev_local_low2 :", prev_local_low2)
+                                print("Current High2 :", current_high2, "Previous High2 :", previous_high2, "local_high2 :", local_high2, " prev_local_high2:", prev_local_high2,"temp_high2 :",temp_high2)
+                                print("Current Low2 :", current_low2, "Previous Low2 :", previous_low2, "local_low2 :", local_low2, " prev_local_low2 :", prev_local_low2,"temp_low2 :",temp_low2)
                                 print("   ")
                                 time.sleep(0.5)
 
@@ -180,8 +191,8 @@ for index1, row1 in data1.iterrows():
                                 # Printing data for data3
 
                                 print("----DAILY----:", current_time3)
-                                print("Current High3 :", current_high3, "Previous High3 :", previous_high3, "local_high3 :", local_high3, " prev_local_high3:",prev_local_high3)
-                                print("Current Low3 :", current_low3, "Previous Low3 :", previous_low3, "local_low3 :", local_low3, " prev_local_low3 :", prev_local_low3)
+                                print("Current High3 :", current_high3, "Previous High3 :", previous_high3, "local_high3 :", local_high3, " prev_local_high3:",prev_local_high3,"temp_high3 :",temp_high3)
+                                print("Current Low3 :", current_low3, "Previous Low3 :", previous_low3, "local_low3 :", local_low3, " prev_local_low3 :", prev_local_low3,"temp_low3 :",temp_low3)
                                 time.sleep(0.5)
 
                                 # updating exit price----------------------------------
